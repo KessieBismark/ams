10a5a4266878ad413c625bb4924fe7e0d1a42ba5
Bismark Kessie <kessiebismark19@gmail.com>
2023-03-22 15:11:17 +0000

    3/22/2023

--- lib/services/constants/server.dart
+++ lib/services/constants/server.dart
@@ -1,7 +1,7 @@
 class Api {
   //static const url = "https://ams.royalfoamghana.com/server/query.php";
- // static const url = "https://ams.royalfoamghana.com/server/query.php";
-  static const url = "http://localhost/ams/query.php";
+  static const url = "https://ams.royalfoamghana.com/server/query.php";
+ // static const url = "http://localhost/ams/query.php";
   final Uri bistech = Uri.parse('https://bistechgh.com/');
   final Uri downloadLink = Uri.parse('https://bistechgh.com/');
 }
